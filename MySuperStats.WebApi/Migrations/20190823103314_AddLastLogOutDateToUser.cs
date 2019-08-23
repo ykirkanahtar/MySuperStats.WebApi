ff72e866443fc2a5015d7a67d897634bffb6ca11
Yunus Emre <yunusemre.kirkanahtar@cutesafe.com>
2019-08-23 15:31:47 +0300

    Çeşitli değişiklikler yapıldı

---
+++ MySuperStats.WebApi/Migrations/20190823103314_AddLastLogOutDateToUser.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MySuperStats.WebApi.Migrations
+{
+    public partial class AddLastLogOutDateToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "last_log_out_date",
+                table: "users",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "last_token_date",
+                table: "users",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.UpdateData(
+                table: "roles",
+                keyColumn: "id",
+                keyValue: 1,
+                column: "concurrency_stamp",
+                value: "35a86284-0cd9-4058-893c-506dd0789ef0");
+
+            migrationBuilder.UpdateData(
+                table: "roles",
+                keyColumn: "id",
+                keyValue: 2,
+                column: "concurrency_stamp",
+                value: "28287e76-b84c-4e5e-9b2a-ada6c4cf7cfb");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "last_log_out_date",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "last_token_date",
+                table: "users");
+
+            migrationBuilder.UpdateData(
+                table: "roles",
+                keyColumn: "id",
+                keyValue: 1,
+                column: "concurrency_stamp",
+                value: "13a18943-e95b-403b-8feb-98b7493abf70");
+
+            migrationBuilder.UpdateData(
+                table: "roles",
+                keyColumn: "id",
+                keyValue: 2,
+                column: "concurrency_stamp",
+                value: "4b02fffb-cfa5-45ac-9b5c-76db24a86125");
+        }
+    }
+}
