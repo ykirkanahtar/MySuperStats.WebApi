5337078c02850438e0da7102577921da5a3408b2
Yunus Emre <yunusemre.kirkanahtar@cutesafe.com>
2019-10-17 09:54:51 +0300

    Giriş sayfasına futbol istatistiklerinin gösterimi de eklendi

--- MySuperStats.WebUI/Pages/Index.cshtml.cs
+++ MySuperStats.WebUI/Pages/Index.cshtml.cs
@@ -7,10 +7,12 @@ using System.Threading;
 using System.Threading.Tasks;
 using CS.Common.WebApi.Connector;
 using CustomFramework.WebApiUtils.Contracts;
+using CustomFramework.WebApiUtils.Contracts.Resources;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using MySuperStats.Contracts.Enums;
 using MySuperStats.Contracts.Responses;
 using MySuperStats.WebUI.ApplicationSettings;
 using MySuperStats.WebUI.Constants;
@@ -23,10 +25,14 @@ namespace MySuperStats.WebUI.Pages
     {
         private readonly IWebApiConnector<ApiResponse> _webApiConnector;
         private readonly AppSettings _appSettings;
-        public readonly ISession _session;
+        private readonly ISession _session;
+        private readonly ILocalizationService _localizer;
 
-        public UserDetailWithBasketballStatResponse PlayerStats { get; set; }
+        public UserDetailWithBasketballStatResponse PlayerBasketballStats { get; set; }
+        public UserDetailWithFootballStatResponse PlayerFootballStats { get; set; }
         public List<BasketballStatResponse> BasketballStats { get; set; }
+        public List<FootballStatResponse> FootballStats { get; set; }
+
 
         [BindProperty]
         public List<SelectListItem> MatchGroups { get; set; }
@@ -34,15 +40,23 @@ namespace MySuperStats.WebUI.Pages
         [BindProperty]
         public int MatchGroupId { get; set; }
 
-        public IndexModel(ISession session, IWebApiConnector<ApiResponse> webApiConnector, AppSettings appSettings)
+        [BindProperty]
+        public MatchGroupResponse MatchGroup { get; set; }
+
+        [BindProperty]
+        public PlayerResponse Player { get; set; }
+
+        public IndexModel(ISession session, IWebApiConnector<ApiResponse> webApiConnector, AppSettings appSettings, ILocalizationService localizer)
         {
             _session = session;
             _webApiConnector = webApiConnector;
             _appSettings = appSettings;
-            PlayerStats = new UserDetailWithBasketballStatResponse();
+            PlayerBasketballStats = new UserDetailWithBasketballStatResponse();
+            PlayerFootballStats = new UserDetailWithFootballStatResponse();
             MatchGroups = new List<SelectListItem>();
 
             InitializePage();
+            _localizer = localizer;
         }
 
         private void InitializePage()
@@ -85,18 +99,49 @@ namespace MySuperStats.WebUI.Pages
         {
             try
             {
-                var loggedPlayer = SessionUtil.GetLoggedUser(_session).Player;
+                Player = SessionUtil.GetLoggedUser(_session).Player;
 
-                var getUrl = $"{_appSettings.WebApiUrl}{String.Format(ApiUrls.GetPlayerWithBasketballStats, loggedPlayer.Id, MatchGroupId)}";
-                var response = await _webApiConnector.GetAsync(getUrl, culture, SessionUtil.GetToken(_session));
+                if (MatchGroups.Count == 0)
+                {
+                    TempData["ErrorMessage"] = _localizer.GetValue("You are not a member of an match group yet");
+                    return;
+                }
 
+                var matchGroupGetUrl = $"{_appSettings.WebApiUrl}{String.Format(ApiUrls.GetMatchGroupById, MatchGroupId)}";
+                var response = await _webApiConnector.GetAsync(matchGroupGetUrl, culture, SessionUtil.GetToken(_session));
                 if (response.StatusCode == HttpStatusCode.OK)
                 {
-                    PlayerStats = JsonConvert.DeserializeObject<UserDetailWithBasketballStatResponse>(response.Result.ToString());
-                    BasketballStats = PlayerStats.Player.BasketballStats.OrderBy(p => p.Match.MatchDate).ThenBy(p => p.Match.Order).ToList();
+                    MatchGroup = JsonConvert.DeserializeObject<MatchGroupResponse>(response.Result.ToString());
                 }
                 else
                     throw new Exception(response.Message);
+
+                if (MatchGroup.MatchGroupType == MatchGroupType.Basketball)
+                {
+                    var basketballStatsGetUrl = $"{_appSettings.WebApiUrl}{String.Format(ApiUrls.GetPlayerWithBasketballStats, Player.Id, MatchGroupId)}";
+                    var basketballStatsResponse = await _webApiConnector.GetAsync(basketballStatsGetUrl, culture, SessionUtil.GetToken(_session));
+
+                    if (basketballStatsResponse.StatusCode == HttpStatusCode.OK)
+                    {
+                        PlayerBasketballStats = JsonConvert.DeserializeObject<UserDetailWithBasketballStatResponse>(basketballStatsResponse.Result.ToString());
+                        BasketballStats = PlayerBasketballStats.Player.BasketballStats.OrderBy(p => p.Match.MatchDate).ThenBy(p => p.Match.Order).ToList();
+                    }
+                    else
+                        throw new Exception(basketballStatsResponse.Message);
+                }
+                else if (MatchGroup.MatchGroupType == MatchGroupType.Football)
+                {
+                    var footballStatsGetUrl = $"{_appSettings.WebApiUrl}{String.Format(ApiUrls.GetPlayerWithFootballStats, Player.Id, MatchGroupId)}";
+                    var footballStatsResponse = await _webApiConnector.GetAsync(footballStatsGetUrl, culture, SessionUtil.GetToken(_session));
+
+                    if (footballStatsResponse.StatusCode == HttpStatusCode.OK)
+                    {
+                        PlayerFootballStats = JsonConvert.DeserializeObject<UserDetailWithFootballStatResponse>(footballStatsResponse.Result.ToString());
+                        FootballStats = PlayerFootballStats.Player.FootballStats.OrderBy(p => p.Match.MatchDate).ThenBy(p => p.Match.Order).ToList();
+                    }
+                    else
+                        throw new Exception(footballStatsResponse.Message);
+                }
             }
             catch (Exception ex)
             {
